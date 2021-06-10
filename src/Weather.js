c75608b36e57fca1606a19b5679b5bc9f437460f
brenann-lsf <brenannfrancis@gmail.com>
2021-06-09 21:31:55 -0400

    Added all Weather Info inside a new component

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
+
 export default function Weather (props){
 const[weatherData, setWeatherData]=useState({ready:false});
 function handleResponse(response){
@@ -38,34 +39,8 @@ if (weatherData.ready){
                 </div>
             </div>
         </form>
-        <h1>{weatherData.city}</h1>
-        <ul>
-            <li><FormattedDate date={weatherData.date}/></li>
-            <li className="text-capitalize">{weatherData.description}</li>
-        </ul>
-        <div className="row mt-3">
-            <div className="col-6">
-            <div className="clearfix">
-                <img 
-                src={weatherData.iconUrl}
-                alt={weatherData.description}
-                className="float-left"/>
-            <div className="float-left">
-                <span className="temperature">{Math.round(weatherData.temperature)}</span>
-                <span className="unit">°C</span>
-            </div>
-            </div>
-            </div>
-            
-            <div className="col-6">
-            <ul>
-              
-                <li>Humidity: {weatherData.humidity}%</li>
-                <li>Wind: {weatherData.wind}km/h</li>
-            </ul>
-            </div>
+        <WeatherInfo data={weatherData}/>
         </div>
-        </div>   
     );
 } else{
     const apiKey=`4fce99a02cfc536e13a3dc6fb2622c7d`;
