441a0c0410d7a10de2f636614ccf038b7f139d0b
brenann-lsf <brenannfrancis@gmail.com>
2021-06-14 19:40:35 -0400

    Built Forecast Layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 
 import './App.css';
 import Weather from "./Weather.js";
-
+import WeatherForecast from "./WeatherForecast.js";
 
 export default function App() {
   return (
     <div className="App">
     <div className="container">
       <Weather defaultCity="Toronto"/>
+      <WeatherForecast />
     </div>
     <footer>
       This project was coded by Brenann Francis and is{" "}
