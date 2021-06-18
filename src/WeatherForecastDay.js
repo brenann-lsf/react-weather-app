22cb6d331cfc352324e19ead4a5c9d2d45a105ec
brenann-lsf <brenannfrancis@gmail.com>
2021-06-17 20:56:14 -0400

    Added a Loop to Forecast & Removed Fahrenheit Conversion

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -21,10 +21,10 @@ export default function WeatherForecastDay(props){
         <div className="WeatherForecast-day">
             {day()}
          </div>
-        <WeatherIcon code={props.data.weather[0].icon} size={36}/>
+        <WeatherIcon code={props.data.weather[0].icon} size={10}/>
         <div className="WeatherForecast-temperatures">
-            <span className="WeatherForecast-temperature-max">{maxTemperature()}°</span>
-             <span className="WeatherForecast-temperature-min">{minTemperature()}°</span>
+            <span className="WeatherForecast-temperature-max">{maxTemperature()}</span>
+             <span className="WeatherForecast-temperature-min">{minTemperature()}</span>
         </div> 
         </div>
     );           
